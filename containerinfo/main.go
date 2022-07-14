e93f6cf9aea186c1fb0cdaa4aa04b771584d7f6f
Pedro Belem <pbelem1995@gmail.com>
2022-07-14 22:25:58 +0100

    Initialize k8s clientset

--- containerinfo/main.go
+++ containerinfo/main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"log"
 	"net/http"
 )
 
+func initializeClientset() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
+}
+
 func main() {
+	_, err := initializeClientset()
+	if err != nil {
+		log.Fatal("Error initializing kubernetes clientset")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello world")
 	})
